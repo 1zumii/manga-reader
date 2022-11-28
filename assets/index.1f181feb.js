f236433f890f2b0309688316ede2f0f51fa58b42
yqllin <524123601@qq.com>
2022-11-28 19:37:19 +0800

    Updates

--- assets/index.1f181feb.js
+++
@@ -1,1 +0,0 @@
-import{g as a,o as s}from"./index.3450cacd.js";const e=()=>{const o=a();return s(()=>{console.log(o.id)}),[]};export{e as default};
---
+++ assets/index.fc820c7f.js
@@ -0,0 +1,1 @@
+import{g as a,o as s}from"./index.2a058f5e.js";const e=()=>{const o=a();return s(()=>{console.log(o.id)}),[]};export{e as default};
