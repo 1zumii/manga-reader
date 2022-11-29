66a92fe5f3b9936987c50c8d8076859db9f6ca5a
izumiiAoba <39220621+izumiiAoba@users.noreply.github.com>
2022-11-29 17:18:43 +0000

    Deploying to gh-pages from @ izumiiAoba/manga-reader@b90ccf8dfbed7e7b61b50c0905777529d5bb60c1 🚀

--- assets/index.1c75ca2e.js
+++
@@ -1,1 +0,0 @@
-import{h as a,o as s}from"./index.e39a9d2a.js";const e=()=>{const o=a();return s(()=>{console.log(o.id)}),[]};export{e as default};
---
+++ assets/index.e877fd36.js
@@ -0,0 +1,1 @@
+import{h as a,o as s}from"./index.be8922cd.js";const e=()=>{const o=a();return s(()=>{console.log(o.id)}),[]};export{e as default};
